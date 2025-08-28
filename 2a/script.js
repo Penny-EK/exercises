af9477c0f25759583f4aa423419067fb8c8599b4
pekr0002 <pekr0002@stud.kea.dk>
2025-08-28 08:58:37 +0200

    rockpaperscissors

---
+++ 2a/script.js
@@ -0,0 +1,5 @@
+presentPet("Penny", "dog", "Berta");
+
+function presentPet(firstName, animalType, animalName) {
+  console.log(`My name is ${firstName}, I have a ${animalType} called ${animalName}`);
+}
