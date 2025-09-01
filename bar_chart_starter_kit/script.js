b2e263e5f4a0b2d0b0e205cc3a171f75f2776bbd
pekr0002 <pekr0002@stud.kea.dk>
2025-09-01 14:48:28 +0200

    graph done

--- bar_chart_starter_kit/script.js
+++ bar_chart_starter_kit/script.js
@@ -1,5 +1,18 @@
 const list = document.querySelector("ul");
+const fData = [];
+setInterval(updateGraph, 1000);
 
-const li = document.createElement("li");
-li.style.setProperty("--height", "30");
-list.appendChild(li);
+function updateGraph() {
+  const newData = Math.ceil(Math.random() * 100);
+
+  if (fData.length == 20) {
+    fData.shift();
+    document.querySelector("li").remove();
+  }
+  fData.push(newData);
+  const li = document.createElement("li");
+  li.style.setProperty("--height", newData);
+  list.appendChild(li);
+
+  console.log(fData);
+}
