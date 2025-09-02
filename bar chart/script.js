93931b1c007f57e21a97f7e2d125ca56ec2493cc
pekr0002 <pekr0002@stud.kea.dk>
2025-09-02 13:38:15 +0200

    2 solve

--- bar chart/script.js
+++ bar chart/script.js
@@ -1,3 +1,5 @@
+// løsning 1
+
 const list = document.querySelector("ul");
 const fData = [];
 setInterval(updateGraph, 1000);
@@ -16,3 +18,23 @@ function updateGraph() {
 
   console.log(fData);
 }
+
+// løsning 2
+// husk at udkommenter li i html
+// const list = document.querySelector("ul");
+// const barArr = [];
+// setInterval(generateBars, 1000);
+
+// function generateBars() {
+//   barArr.push(Math.round(Math.random() * 100));
+//   if (barArr.length > 20) {
+//     barArr.shift();
+//   }
+//   render();
+// }
+// function render() {
+//   document.querySelectorAll("li").forEach((elm, i) => {
+//     console.log("i", i);
+//     elm.style.setProperty("--height", barArr[i]);
+//   });
+// }
